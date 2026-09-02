aa41d2b8ba99a1866fe2795d0325049bb708607d
jeffery945 <95352831+jeffery945@users.noreply.github.com>
2026-09-01 22:36:42 -0400

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        counts, countt = {}, {}
+        if t == "": return ""
+
+        for i in range(len(t)):
+            countt[t[i]] = 1 + countt.get(t[i], 0)
+        i, j = 0, 0
+        have, need = 0, len(countt)
+        res = [-1, 1]
+        reslength = float("infinity")
+        while j < len(s):
+            counts[s[j]] = 1 + counts.get(s[j], 0)
+            if s[j] in countt and countt[s[j]] == counts[s[j]]:
+                have += 1
+            
+            while have == need:
+                if j - i + 1 < reslength:
+                    res = [i, j]
+                    reslength = j - i + 1
+
+                # pop left to try that if there is smaller one
+                counts[s[i]] -= 1
+                if s[i] in countt and counts[s[i]] < countt[s[i]]:
+                    have -= 1
+                i += 1
+            j += 1
+
+        i, j = res
+        return s[i:j + 1] if reslength != float("infinity") else ""
